aa10e9a80a7a8b49c29d762cc57dc5e8e1cfe2dc
Hyeongsub Lee <hslee1@seegene.com>
2021-08-17 17:07:48 +0900

    feat: convert async => sync

--- src/fileMapping.ts
+++ src/fileMapping.ts
@@ -23,51 +23,45 @@ const fileMapping = async (files: any[]) => {
     for (let i = 0; i < pattern.length; i += 1) {
       if (pattern[i].test(files[j].name)) {
         // let index=fileTurn[i];
-        const fileReader = new FileReader();
         const fileName = files[j].name;
         console.log(fileName, "읽기");
 
-        fileReader.readAsBinaryString(files[j]);
-        fileReader.onload = async (event) => {
-          // typescript에서 null이 될 가능성을 있다고 판단하기에 처리해야함.
-          const data = event.target ? event.target.result : null;
-          if (data != null) {
-            const workbook = xlsx.read(data, { type: "binary" });
-            // console.log(workbook);
-            const sheetNum = workbook.SheetNames.length;
+        const data = await readSynchronous(files[j]);
+        const workbook = xlsx.read(data, { type: "binary" });
+        console.log(workbook);
 
-            const sheetArr = new Array(sheetNum);
+        const sheetNum = workbook.SheetNames.length;
+        const sheetArr = new Array(sheetNum);
 
-            // const channelData = new Array<string>(sheetNum);
-            for (let j = 0; j < sheetNum; j += 1) {
-              // channelData[j]=workbook.SheetNames[j];
-              //  const rowObject = xlsx.utils.sheet_to_row_object_array(
-              //    wholeExcel.Sheets[wholeExcel.SheetNames[j]],
-              //  );
-              sheetArr[j] = xlsx.utils.sheet_to_row_object_array(
-                workbook.Sheets[workbook.SheetNames[j]]
-              );
-            }
-            fileTurn[i] = JSON.parse(JSON.stringify(sheetArr));
-            // console.log("sheetArr출력",0,"는",fileTurn[i][0][0])
-          } else {
-            console.log("data가 null");
-          }
-          // console.log("sheetArr출력", 1, "는", fileTurn[0][0][0]);
-        };
-        console.log("file안 ....sheetArr출력", fileTurn);
-        // console.log("sheetArr출력", 2, "는", fileTurn[0][0][0]);
+        for (let k = 0; k < sheetNum; k += 1) {
+          sheetArr[k] = xlsx.utils.sheet_to_row_object_array(
+            workbook.Sheets[workbook.SheetNames[k]]
+          );
+        }
+        fileTurn[i] = JSON.parse(JSON.stringify(sheetArr));
+        console.log("sheetArr출력", 0, "는", fileTurn[i][0][0]);
         break;
       }
       if (i === pattern.length - 1) console.log("not mapping name");
     }
   }
+
   // console.log("sheetArr출력", 0, "는", fileTurn[0][0][0]);
   // console.log("sheetArr출력", 1, "는", fileTurn[1][0][0]);
-  // console.log("data는", fileTurn);
-  // console.log("fileTurn은",fileTurn)
-  // console.log(fileTurn[0]);
+  console.log("data는", fileTurn);
+  console.log("fileTurn은", fileTurn);
+  console.log(fileTurn[6]);
   return fileTurn;
+};
+
+const readSynchronous = async (file: any) => {
+  let resultBinary = await new Promise((resolve) => {
+    let fileReader = new FileReader();
+    fileReader.onload = (e) => resolve(fileReader.result);
+    fileReader.readAsBinaryString(file);
+  });
+
+  return resultBinary;
 };
 
 export { fileMapping };
